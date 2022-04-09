db4260f77419eca800b5313ff680613d2e300a6e
SergiyRybin <serhiobax@gmail.com>
2022-04-09 17:33:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,21 +10,33 @@ let nameCountry = null
 
 const searchBox = document.querySelector("#search-box")
 searchBox.addEventListener("input", (event)=>{
-  nameCountry = event.currentTarget.value
+  nameCountry = event.currentTarget.value.trim()
   fetchUser(nameCountry).then(renderCountry)
   // searchBox.reset()
 
   function renderCountry(countrys){
-console.log(countrys.length)
+    console.log(countrys[0].languages)
+
+
+
 if(countrys.length===1){
-  const markup = countrys.map(({name, flags, area })=>{
-    return `<li style="display: flex;align-items: center;"><img src="${flags.png}" alt="${name.official}" width='25px'>${name.official}</li>Area: ${area}<p></p>`
+
+  const markup = countrys.map(({name, flags, capital, population, languages})=>{
+
+    return `<li style="display: flex;align-items: center;"><img src="${flags.svg}" alt="${name.official}" width='25px'>${name.official}</li><p>Capital: ${capital}</p><p>Population: ${population}</p><p>Languages: ${languages}</p>`
   }).join("");
   listCountry.innerHTML=markup
 }else{
+
+// const lang = countrys.languages.map(({languages})=>{
+// return console.log(languages)
+// }).join("")
+// console.log(lang)
+
   const markup = countrys.map(({name, flags})=>{
-    return `<li style="display: flex;align-items: center;"><img src="${flags.png}" alt="${name.official}" width='25px'>${name.official}</li>`
+    return `<li style="display: flex;align-items: center;"><img src="${flags.svg}" alt="${name.official}" width='25px'>${name.official}</li>`
   }).join("");
+con
   listCountry.innerHTML=markup
     }
 }
