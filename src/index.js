56b109c472c727bfd6af9eff0598a2bdd57edbb7
SergiyRybin <serhiobax@gmail.com>
2022-04-09 12:15:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+// let name = "peru"
+
+fetch("https://restcountries.com/v3.1/name/{name}")
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log(error));
