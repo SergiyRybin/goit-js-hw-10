ab0b4b4f5936156e327920ba370d09a7f0bbd5d5
SergiyRybin <serhiobax@gmail.com>
2022-04-10 14:20:16 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,9 @@
 export function fetchUser(name){
     return fetch(`https://restcountries.com/v3.1/name/${name}`)
     .then(response =>{
-        if (!response.ok){ return Promise.reject(response)}
-           else{ return response.json()}
+        if (!response.ok) throw Error(response.statusText)
+        // { return Promise.reject(response)}
+          return response.json()
         // if(response.status===404){
         //     Promise.reject()
         // }
