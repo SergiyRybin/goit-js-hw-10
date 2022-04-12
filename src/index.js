46385b10090d0cd0a58db940c535ecb2c4187ee5
SergiyRybin <serhiobax@gmail.com>
2022-04-12 18:39:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
 const listCountry = document.querySelector(".country-list")
 
-let nameCountry = {};
+let nameCountry = '';
 const searchBox = document.querySelector("#search-box")
 
 searchBox.addEventListener("input", debounce(onText, DEBOUNCE_DELAY))
@@ -26,16 +26,20 @@ function renderCountry(countrys){
 
 if(countrys.length===1){
 const markup = countrys.map(({name, flags, capital, population, languages})=>{
-
-  return `<li style="display: flex;align-items: center;"><img src="${flags.svg}" alt="${name.official}" width='25px'>${name.official}</li><p>Capital: ${capital}</p><p>Population: ${population}</p><p>Languages: ${Object.values(languages)}</p>`
+return `<li style="display: flex;align-items: center;"><img src="${flags.svg}" alt="${name.official}" width='25px'>${name.official}</li><p>Capital: ${capital}</p><p>Population: ${population}</p><p>Languages: ${Object.values(languages)}</p>`
 }).join("");
-listCountry.innerHTML=markup}
-else{const markup = countrys.map(({name, flags})=>{
+listCountry.innerHTML=markup
+}
+if(countrys.length>10){
+  return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
+}
+  if(countrys.length>=2 && countrys.length<=10){
+  const markup = countrys.map(({name, flags})=>{
   return `<li style="display: flex;align-items: center;"><img src="${flags.svg}" alt="${name.official}" width='25px'>${name.official}</li>`
 }).join("");
 listCountry.innerHTML=markup
 Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-}
+  }
 }
 
 function renderEror(){
